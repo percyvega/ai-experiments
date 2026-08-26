351db976030db919c5be2732389c057034c8e508
Percy Vega <percyvega@gmail.com>
2026-08-25 23:54:37 -0400

    Minor improvement. Much bigger introduction-to-java.txt file

--- src/test/java/com/percyvega/langchain4j/T9CompareEmbeddings.java
+++ src/test/java/com/percyvega/langchain4j/T9CompareEmbeddings.java
@@ -19,12 +19,12 @@ class T9CompareEmbeddings {
 
     public static final String COMMAND_PROMPT = "\nAsk me something about Java> ";
 
-    private record Comparison(String greeting, float cosine, float euclidean) {
+    private record Comparison(String sentence, float cosine, float euclidean) {
     }
 
     // Interactive, so it is a main() rather than a @Test: a test runner gives us no console to read from.
     void main() {
-        Map<String, Embedding> greetingVectors = EmbeddingUtils.getEmbeddings(FileUtils.getSentences(FILE_NAME));
+        Map<String, Embedding> embeddingMap = EmbeddingUtils.getEmbeddings(FileUtils.getSentences(FILE_NAME));
 
         for (String userInput = IO.readln(COMMAND_PROMPT); !userInput.isEmpty(); userInput = IO.readln(COMMAND_PROMPT)) {
 
@@ -32,7 +32,7 @@ class T9CompareEmbeddings {
 
             log.info("Calculating relatedness to \"" + userInput + "\"");
             // compare userInput vector with each calculated vector
-            List<Comparison> comparisons = greetingVectors.entrySet().stream()
+            List<Comparison> comparisons = embeddingMap.entrySet().stream()
                     .map(embeddingEntry -> new Comparison(embeddingEntry.getKey(),
                             cosineSimilarity(inputVector, embeddingEntry.getValue().vector()),
                             euclideanSimilarity(inputVector, embeddingEntry.getValue().vector())))
@@ -42,7 +42,7 @@ class T9CompareEmbeddings {
 
             log.info("COSINE | EUCLIDEAN | SENTENCE");
             comparisons.forEach(comparison -> log.info(String.format("%6.4f |    %6.4f | %s",
-                    comparison.cosine(), comparison.euclidean(), comparison.greeting())));
+                    comparison.cosine(), comparison.euclidean(), comparison.sentence())));
         }
     }
 
